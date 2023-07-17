8bb445547ac0fc330f777bcd74ef4c8977cf31f6
ChealsT <tanchealsea@gmail.com>
2023-07-17 15:28:20 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,21 @@
 MINIMUM = 0
-password = input("Password: ")
 
-while password <= MINIMUM:
-    print("Invalid password")
+
+def main():
+    password = get_password(MINIMUM)
+    display_stars(password)
+
+
+def display_stars(password):
+    print(len(password) * "*")
+
+
+def get_password(MINIMUM):
     password = input("Password: ")
+    while password <= MINIMUM:
+        print("Invalid password")
+        password = input("Password: ")
+    return password
 
-print(len(password) * "*")
+
+main()
