2170fcf4d090d9d07fa3366fbfeeb0cd1c468b06
ChealsT <tanchealsea@gmail.com>
2023-07-17 15:49:52 +0800

    Add docstrings to all functions in password_stars

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,17 +2,20 @@ MINIMUM = 0
 
 
 def main():
+    """Get password to display stars."""
     password = get_password(MINIMUM)
     display_stars(password)
 
 
 def display_stars(password):
+    """Print the stars."""
     print(len(password) * "*")
 
 
 def get_password(MINIMUM):
+    """Validate input with error-checking."""
     password = input("Password: ")
-    while password <= MINIMUM:
+    while len(password) <= MINIMUM:
         print("Invalid password")
         password = input("Password: ")
     return password
