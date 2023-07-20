19cbbed497df83d62dc454718b6c4a3427fd2ce5
ChealsT <tanchealsea@gmail.com>
2023-07-20 14:44:07 +0800

    Rewrote function names in temperatures.py

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -8,11 +8,11 @@ def main():
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = get_fahrenheit_from_celsius(celsius)
+            fahrenheit = get_celsius_convert_to_fahrenheit(celsius)
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = get_celsius_from_fahrenheit(fahrenheit)
+            celsius = get_fahrenheit_convert_to_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} C")
         else:
             print("Invalid option")
@@ -20,11 +20,11 @@ def main():
         choice = input(">>> ").upper()
     print("Thank you.")
 
-def get_fahrenheit_from_celsius(celsius):
+def get_celsius_convert_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit."""
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
-def get_celsius_from_fahrenheit(fahrenheit):
+def get_fahrenheit_convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius."""
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
