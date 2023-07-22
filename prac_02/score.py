abd7245250e624aa22619809dedad4648a21918b
ChealsT <tanchealsea@gmail.com>
2023-07-22 10:36:12 +0800

    Add score check program

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,19 +1,34 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+import random
+
 MAXIMUM_VALUE = 100
 MINIMUM_VALUE = 0
 EXCELLENT_THRESHOLD = 90
 PASSABLE_THRESHOLD = 50
 
-score = float(input("Enter score: "))
 
-if score > MAXIMUM_VALUE or score < MINIMUM_VALUE:
-    print("Invalid score")
-elif score >= EXCELLENT_THRESHOLD:
-    print("Excellent")
-elif score >= PASSABLE_THRESHOLD:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Get score to determine grade tier."""
+    score = float(input("Enter score: "))
+    result = give_grades(score)
+    print(result)
+
+    another_score = (random.randint(0, 100))
+    print(another_score)
+    another_result = give_grades(another_score)
+    print(another_result)
+
+
+def give_grades(score):
+    """Use score to determine grade tier."""
+    if score > MAXIMUM_VALUE or score < MINIMUM_VALUE:
+        result = "Invalid score"
+    elif score >= EXCELLENT_THRESHOLD:
+        result = "Excellent"
+    elif score >= PASSABLE_THRESHOLD:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
