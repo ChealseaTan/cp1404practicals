920d481a0d89a3e42dfaaf634dc6e1bcefecd70d
ChealsT <tanchealsea@gmail.com>
2023-07-24 13:54:24 +0800

    Add random numbers program

---
+++ prac_03/randoms.py
@@ -0,0 +1,24 @@
+import random
+# 15 12 12 19 11 15 11 17 16 6 17
+# 7 9 3 7 3 5 9 5 5 5
+# 5.205020984169328 4.059792757854038 3.975952385341377 4.645149384848026 5.155356132049977 2.900844654547589
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# random numbers from the range
+# smallest is 5, largest is 20
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# only odd numbers from the range
+# smallest is 3, largest is 9
+# no
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# decimal numbers from the range
+# smallest is 2.5, largest is 5.5
+
+print(random.randint(1, 100))
+
