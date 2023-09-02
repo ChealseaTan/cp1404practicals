2dcfe8d05f2ab639b1c37b26fcd851c7c7cd58b4
ChealsT <tanchealsea@gmail.com>
2023-09-02 22:07:37 +0800

    Add read file, sort and print result

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,4 +8,15 @@ from prac_07.guitar import Guitar
 
 FILENAME = "guitars.csv"
 
+guitars = []
+with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+    for i, line in enumerate(in_file, 0):
+        name, year, cost = line.strip().split(",")
+        guitars.append((Guitar(name, year, cost)))
+        print(guitars[i])
 
+guitars.sort()
+print("\nSorted by year:")
+
+for guitar in guitars:
+    print(guitar)
