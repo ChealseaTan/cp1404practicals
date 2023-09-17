16ca59e109edcd61aa626e60699a316a9de14f23
ChealsT <tanchealsea@gmail.com>
2023-09-17 12:13:38 +0800

    Fix play function error and add docstrings

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,18 +1,22 @@
+
 class Band:
-    def __init__(self, mode=""):
-        self.mode = mode
+    def __init__(self, name=""):
+        """Initiate a Band instance."""
+        self.name = name
         self.members = []
 
     def __str__(self):
-        return f"{self.mode} ({', '.join(str(member) for member in self.members)})"
-
-    def __repr__(self):
-        """Return a string representation of a Musician, showing the variables."""
-        return str(vars(self))
+        """Return band string."""
+        return f"{self.name} ({', '.join(str(member) for member in self.members)})"
 
     def add(self, member):
+        """Add a member to the band."""
         return self.members.append(member)
 
     def play(self):
-        for self.member in self.members:
-            return f"{self.member}"
+        """Display the band members playing their instrument."""
+        result = ""
+        for member in self.members:
+            result += member.play() # + "\n"
+            print("\n")
+        return result
