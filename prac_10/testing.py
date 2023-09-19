c399d517973dcd4461fc36b81d9b2436953517c6
ChealsT <tanchealsea@gmail.com>
2023-09-19 17:16:28 +0800

    Complete TODO 3 & 4

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -12,17 +12,17 @@ def repeat_string(s, n):
     return " ".join([s] * n)
 
 
-# def is_long_word(word, length=5):
-#     """
-#     Determine if the word is as long or longer than the length passed in
-#     >>> is_long_word("not")
-#     False
-#     >>> is_long_word("supercalifrag")
-#     True
-#     >>> is_long_word("Python", 6)
-#     True
-#     """
-#     return len(word) > length
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
 
 
 def run_tests():
@@ -39,7 +39,7 @@ def run_tests():
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
     test_car = Car()
-    assert test_car._odometer == 0, "Car does not set odometer correctly"
+    assert test_car.odometer == 0, "Car does not set odometer correctly"
 
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
@@ -54,7 +54,7 @@ run_tests()
 
 # TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
 # TODO: 4. Fix the failing is_long_word function
 # (don't change the tests, change the function!)
