37abf42c32ebdca657de149d1b08f2ad732e11d3
ChealsT <tanchealsea@gmail.com>
2023-09-19 17:12:39 +0800

    Complete TODO 2

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -12,17 +12,17 @@ def repeat_string(s, n):
     return " ".join([s] * n)
 
 
-def is_long_word(word, length=5):
-    """
-    Determine if the word is as long or longer than the length passed in
-    >>> is_long_word("not")
-    False
-    >>> is_long_word("supercalifrag")
-    True
-    >>> is_long_word("Python", 6)
-    True
-    """
-    return len(word) > length
+# def is_long_word(word, length=5):
+#     """
+#     Determine if the word is as long or longer than the length passed in
+#     >>> is_long_word("not")
+#     False
+#     >>> is_long_word("supercalifrag")
+#     True
+#     >>> is_long_word("Python", 6)
+#     True
+#     """
+#     return len(word) > length
 
 
 def run_tests():
@@ -45,7 +45,9 @@ def run_tests():
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
+    assert test_car.fuel == 0, "Car does not set fuel correctly"
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10, "Car does not set fuel correctly"
 
 
 run_tests()
